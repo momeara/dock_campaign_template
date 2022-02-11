9080a2b471ae3b1efd89ad4378cfeb31ffd7a2a5
Matthew O'Meara <maom@umich.edu>
2022-02-11 12:53:44 -0500

    fix syntax for using getposes_blazing_faster.py

--- docking_runs/TEMPLATE_PARAMETER_SCAN/1_run.sh
+++ docking_runs/TEMPLATE_PARAMETER_SCAN/1_run.sh
@@ -15,7 +15,6 @@ DATABASE=$(readlink -f ../../databases/<database_id>)
 source ${DOCK_TEMPLATE}/scripts/dock_clean.sh
 
 echo "Running dock ..."
-echo "Running dock ..."
 for params in ${PREPARED_STRUCTURE}/combo_directories/*/ ; do
     echo "Submitting job for param set: ${param}"
 
@@ -32,7 +31,7 @@ done
 echo "Collecint dock results ..."
 find results -maxdepth 2 -mindepth 2 -type d > dirlist
 python ${DOCKBASE}/analysis/extract_all_blazing_fast.py dirlist extract_all.txt 10
-python ${DOCKBASE}/analysis/getposes_blazing_faster.py '' extract_all.sort.uniq.txt 500 poses.mol2
+python ${DOCKBASE}/analysis/getposes_blazing_faster.py '' extract_all.sort.uniq.txt 500 poses.mol2 test.mol2.gz
 
 source ${DOCK_TEMPLATE}/scripts/dock_statistics.sh
 
