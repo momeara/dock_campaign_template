5c2a61db451b25dee940defdfdbb9c2a38c79c63
Manasa Yadavalli <ymanasa@umich.edu>
2023-10-09 11:01:31 -0400

    updates to docking run submit script and post-dock scripts

--- docking_runs/TEMPLATE_COVALENT/2_gather.sh
+++ docking_runs/TEMPLATE_COVALENT/2_gather.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-
+  
 if [ -z ${DOCK_TEMPLATE+x} ]; then
     echo "ERROR: The \${DOCK_TEMPLATE} variable is not set"
     echo "ERROR: Please run 'source setup_dock_environment.sh' in the project root directory"
 fi
 
 echo "Gather dock results ..."
-ls results/ | grep -v joblist | sed "s#^#results/#" > dirlist
-python ${DOCKBASE}/analysis/extract_all_blazing_fast.py dirlist extract_all.txt 10
-python ${DOCKBASE}/analysis/getposes_blazing_faster.py '' extract_all.sort.uniq.txt 500 poses.mol2 test.mol2.gz
+ls -d results/* > dirlist
+sed -i '$ d' dirlist
+python ${DOCKBASE}/analysis/extract_all_blazing_fast.py dirlist extract_all.txt 100
+# python ${DOCKBASE}/analysis/getposes_blazing_faster.py '' extract_all.sort.uniq.txt 1000000 poses.mol2 test.mol2.gz.0 > poses_out.txt
 
-source ${DOCK_TEMPLATE}/scripts/dock_statistics.sh
+# source ${DOCK_TEMPLATE}/scripts/dock_statistics.sh
 
-Rscript ${DOCK_TEMPLATE}/scripts/analysis/gather_pose_features.R --verbose
+# Rscript ${DOCK_TEMPLATE}/scripts/analysis/gather_pose_features.R --verbose
